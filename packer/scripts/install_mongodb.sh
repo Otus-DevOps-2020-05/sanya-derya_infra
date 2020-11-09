c7822c78206c91cc2f4f8d6b286772cd1e162053
Alexsandr Deriabin <sanya.derya.88@gmail.com>
2020-11-09 15:30:29 +0700

    Packer

---
+++ packer/scripts/install_mongodb.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+wget -qO - https://www.mongodb.org/static/pgp/server-3.2.asc | apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+apt-get update
+apt  install -y mongodb-org
+systemctl start mongod
+systemctl enable mongod
